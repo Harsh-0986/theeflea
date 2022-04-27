429bee68edb16d656e33ac11ac51329c92bd6e49
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-04-27 16:01:34 +0530

    Update DropdownBut.jsx

--- components/DropdownBut.jsx
+++ components/DropdownBut.jsx
@@ -1,9 +1,9 @@
-import Rect from "react";
+import React from "react";
 import Dropdown from "@material-tailwind/react/Dropdown"
 import DropdownItem from "@material-tailwind/react/DropdownItem"
 import DropdownLink from "@material-tailwind/react/DropdownLink"
 
-export default function Dropdown() {
+export default function DropdownBut() {
     return (
         <Dropdown
             color="lightBlue"

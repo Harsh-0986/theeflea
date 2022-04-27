62d9e1e6899a002effe96a4e7c844057d7a5b765
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-04-27 16:55:55 +0530

    Initial

--- components/DropdownBut.jsx
+++ components/DropdownBut.jsx
@@ -1,34 +1,34 @@
-import Rect from "react";
-import Dropdown from "@material-tailwind/react/Dropdown"
-import DropdownItem from "@material-tailwind/react/DropdownItem"
-import DropdownLink from "@material-tailwind/react/DropdownLink"
+import React from "react";
+import Dropdown from "@material-tailwind/react/Dropdown";
+import DropdownItem from "@material-tailwind/react/DropdownItem";
+import DropdownLink from "@material-tailwind/react/DropdownLink";
 
-export default function Dropdown() {
-    return (
-        <Dropdown
-            color="lightBlue"
-            placement="bottom-start"
-            buttonText="Dropdown"
-            buttonType="filled"
-            size="regular"
-            rounded={false}
-            block={false}
-            ripple="light"
-        >
-            <DropdownItem color="lightBlue" ripple="light">
-                Action
-            </DropdownItem>
-            <DropdownLink
-                href="#"
-                color={color}
-                ripple="light"
-                onClick={(e) => e.preventDefault()}
-            >
-                Another Action
-            </DropdownLink>
-            <DropdownItem color="lightBlue" ripple="light">
-                Something else
-            </DropdownItem>
-        </Dropdown>
-    )
+export default function DropdownBut() {
+	return (
+		<Dropdown
+			color="lightBlue"
+			placement="bottom-start"
+			buttonText="Dropdown"
+			buttonType="filled"
+			size="regular"
+			rounded={false}
+			block={false}
+			ripple="light"
+		>
+			<DropdownItem color="lightBlue" ripple="light">
+				Action
+			</DropdownItem>
+			<DropdownLink
+				href="#"
+				color={color}
+				ripple="light"
+				onClick={(e) => e.preventDefault()}
+			>
+				Another Action
+			</DropdownLink>
+			<DropdownItem color="lightBlue" ripple="light">
+				Something else
+			</DropdownItem>
+		</Dropdown>
+	);
 }

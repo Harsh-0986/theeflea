da7f74b82c60a1f77f1e9b459a78016a2d6c75fc
Harsh-0986 <harsh.r.shah2102@gmail.com>
2022-04-28 15:08:30 +0530

    Completed

--- components/DisplayPrice.jsx
+++ components/DisplayPrice.jsx
@@ -1,20 +1,44 @@
 import { AiOutlinePlusSquare, AiOutlineMinusSquare } from "react-icons/ai";
 import { useState } from "react";
+import getStripe from "../lib/getStripe";
 
 const DisplayPrice = ({ day }) => {
 	const [qty, setQty] = useState(1);
+
+	const handleCheckout = async () => {
+		const stripe = await getStripe();
+
+		const response = await fetch("/api/stripe", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				name: `${day} Tickets`,
+				qty: qty,
+				price: day == "Day 1" ? 150 : day == "Day 2" ? 250 : 350,
+			}),
+		});
+
+		if (response.statusCode === 500) return;
+
+		const data = await response.json();
+		stripe.redirectToCheckout({ sessionId: data.id });
+	};
+
+	let price;
+	price = day == "Day 1" ? 150 * qty : day == "Day 2" ? 250 * qty : 350 * qty;
 
 	return (
 		<div className="py-8">
 			<div className="font-bold text-md flex justify-between align-center mb-4">
 				<span>Price</span>
-				{day == "Day 1" ? (
+				{/*day == "Day 1" ? (
 					<span>{`${150 * qty} \u20B9`}</span>
 				) : day == "Day 2" ? (
 					<span>{`${250 * qty} \u20B9`}</span>
 				) : (
 					<span>{`${350 * qty} \u20B9`}</span>
-				)}
+				)*/}
+				<span>{`${price} \u20B9`}</span>
 			</div>
 			<hr />
 			<div className="mt-2 flex justify-between align-center text-md font-bold">
@@ -31,7 +55,10 @@ const DisplayPrice = ({ day }) => {
 					/>
 				</div>
 			</div>
-			<button className="w-[100%] bg-blue-500 px-4 py-2 mt-4 rounded-lg text-white font-bold flex justify-center align-center">
+			<button
+				className="w-[100%] bg-blue-500 px-4 py-2 mt-4 rounded-lg text-white font-bold flex justify-center align-center"
+				onClick={handleCheckout}
+			>
 				Book Now
 			</button>
 		</div>
---
+++ pages/api/stripe.js
@@ -0,0 +1,40 @@
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) {
+	if (req.method === "POST") {
+		try {
+			const params = {
+				submit_type: "pay",
+				mode: "payment",
+				payment_method_types: ["card"],
+				billing_address_collection: "auto",
+				line_items: [
+					{
+						price_data: {
+							currency: "inr",
+							product_data: {
+								name: req.body.name,
+							},
+							unit_amount: req.body.price * 100,
+						},
+						adjustable_quantity: {
+							enabled: true,
+							minimum: 1,
+						},
+						quantity: req.body.qty,
+					},
+				],
+				success_url: `${req.headers.origin}/?success=true`,
+				cancel_url: `${req.headers.origin}/?canceled=true`,
+			};
+
+			const session = await stripe.checkout.sessions.create(params);
+
+			res.status(300).json(session);
+		} catch (error) {
+			res.status(500).json({ statusCode: 500, message: error.message });
+		}
+	}
+}
